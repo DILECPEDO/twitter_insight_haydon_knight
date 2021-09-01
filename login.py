4f429f0bc7857a0cf1ad23c519fa28d16455e97e
Dilec Padovani <dilec.padovani@gmail.com>
2021-09-01 13:39:31 +0200

    added requiremts

--- login.py
+++ login.py
@@ -1,7 +1,8 @@
 import tweepy
 import os
-from dotenv import load_dotenv,set_key
-import pandas as pd
+from dotenv import load_dotenv, set_key
+
+
 # used only first time to get the OTP code so we can retrieve (access_token, access_token_secret)
 # def twitter_api_auth():
 #     load_dotenv()
@@ -12,13 +13,14 @@ import pandas as pd
 #     print(access_token)
 #     set_key(".env", "ACCESS_TOKEN", access_token[0])
 #     set_key(".env", "ACCESS_TOKEN_SECRET", access_token[1])
-    
 
-# Instantiation of twitter API client 
+
+# Instantiation of twitter API client
 # To be used every single time
 def twitter_api_login():
     load_dotenv()
-    auth = tweepy.OAuthHandler(os.getenv("API_KEY"),  os.getenv("API_KEY_SECRET"))
-    auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"))
+    auth = tweepy.OAuthHandler(os.getenv("API_KEY"),
+                               os.getenv("API_KEY_SECRET"))
+    auth.set_access_token(os.getenv("ACCESS_TOKEN"),
+                          os.getenv("ACCESS_TOKEN_SECRET"))
     return tweepy.API(auth, wait_on_rate_limit=True)
-
